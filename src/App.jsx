733e880d202b34a5b4c31d486d0450430532dbb9
Ivan <68665931+Eir-Rithsen@users.noreply.github.com>
2025-11-26 12:34:45 -0300

    Set up routing with BrowserRouter and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import CartPage from "./pages/CartPage";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
+}
 
