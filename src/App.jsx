a6a1bcdd2a9b287f3bdae934f40307590dead917
Ivan <68665931+Eir-Rithsen@users.noreply.github.com>
2025-11-26 12:45:02 -0300

    Refactor App component with context providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-
 import Home from "./pages/Home";
-import Products from "./pages/Products";
+import ProductsPage from "./pages/ProductsPage";
 import CartPage from "./pages/CartPage";
+import Login from "./pages/Login";
+import Admin from "./pages/Admin";
+import { ToastContainer } from "react-toastify";
+
+import { AuthProvider } from "./contexts/AuthContext";
+import { CartProvider } from "./contexts/CartContext";
+import { ProductsProvider } from "./contexts/ProductsContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/cart" element={<CartPage />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <AuthProvider>
+      <ProductsProvider>
+        <CartProvider>
+          <BrowserRouter>
+            <Navbar />
+            <main className="container py-4">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/productos" element={<ProductsPage />} />
+                <Route path="/carrito" element={<CartPage />} />
+                <Route path="/login" element={<Login />} />
+                <Route element={<ProtectedRoute />}>
+                  <Route path="/admin" element={<Admin />} />
+                </Route>
+              </Routes>
+            </main>
+          </BrowserRouter>
+          <ToastContainer position="top-right" autoClose={3000} />
+        </CartProvider>
+      </ProductsProvider>
+    </AuthProvider>
   );
 }
-
